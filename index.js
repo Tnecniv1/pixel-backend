00774567f7b289828c51b3208dfd7fb6d08be2a5
vincent <vincent@macbookair.home>
2025-10-13 05:47:37 -0700

    Add debug logging

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// DEBUT - Code de debug temporaire
+import { Alert } from 'react-native';
+
+const originalError = console.error;
+console.error = (...args) => {
+  originalError(...args);
+  const errorMsg = args.map(a => String(a)).join(' ');
+  Alert.alert('ERREUR DÉTECTÉE', errorMsg.substring(0, 500));
+};
+
+if (!__DEV__) {
+  ErrorUtils.setGlobalHandler((error, isFatal) => {
+    Alert.alert(
+      'CRASH IMMINENT',
+      `${error.name}: ${error.message}\n\nStack: ${error.stack?.substring(0, 300)}`,
+      [{ text: 'OK' }]
+    );
+  });
+}
+// FIN - Code de debug temporaire
+
 import { registerRootComponent } from 'expo';
 
 import App from './App';
